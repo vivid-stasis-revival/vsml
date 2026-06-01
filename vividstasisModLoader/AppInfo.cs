fef13f2b0ba72e5ce550895d9a611b0814b63acc
MalNEW <1032173768@qq.com>
2026-06-01 18:34:27 +0800

    添加IPC(进程间通信)支持

---
+++ vividstasisModLoader/AppInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace vividstasisModLoader
+{
+    public static class AppInfo
+    {
+        public const string VERSION = "v0.1.2";
+        public const string CHANGE_LOG = "增加IPC通讯代码用于和TVOClient联动";
+    }
+}
