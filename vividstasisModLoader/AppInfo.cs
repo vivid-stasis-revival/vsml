28cfd094bc0c78ae5c409812e1410739380e2ebb
Narika <drnarika@outlook.com>
2026-08-19 22:52:29 +0800

    v0.1.4 发布：修复对象事件中模组函数无法正确解析的问题

--- vividstasisModLoader/AppInfo.cs
+++ vividstasisModLoader/AppInfo.cs
@@ -1,8 +1,8 @@
-﻿namespace vividstasisModLoader
+namespace vividstasisModLoader
 {
     public static class AppInfo
     {
-        public const string VERSION = "v0.1.3";
-        public const string CHANGE_LOG = "增加force_custom_path强制路径控制与BVO联动";
+        public const string VERSION = "v0.1.4";
+        public const string CHANGE_LOG = "修复对象事件中模组函数无法正确解析的问题，改为与GlobalScripts一起编译";
     }
 }
